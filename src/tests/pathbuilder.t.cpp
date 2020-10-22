55fa102b16975bd5e8040a4f23cdb60b1f668a59
thederek <mail@derek.tech>
2020-10-22 11:50:12 +0100

    Fixed path building and minor refactor

---
+++ src/tests/pathbuilder.t.cpp
@@ -0,0 +1,19 @@
+#include <gtest/gtest.h>
+
+#include "map_maker.h"
+#include "regexer.h"
+
+TEST(PathBuilder, CanBuildPath) {
+    //TODO Find a neater regex that trims the whitespace at the end of the matches
+    Regexer regexer(R"(\s?([a-zA-Z])\s([^a-zA-Z]+\s))");
+
+    auto matches = regexer.search("M 72.732231,91.993675 H 85.979527 L 72.732231,118.39366 Z");
+    ASSERT_EQ(matches.size(), 3);
+    EXPECT_EQ("M 72.732231,91.993675 ", matches.at(0).matchedString());
+
+    ASSERT_EQ(matches.at(0).groups().size(), 2);
+    EXPECT_EQ("M", matches.at(0).groups().at(0));
+    EXPECT_EQ("72.732231,91.993675 ", matches.at(0).groups().at(1));
+
+    EXPECT_TRUE(true);
+}
